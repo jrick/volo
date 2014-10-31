6997d7b2553e02b47e1cf2d4befa0da05752ff99
Josh Rickmar <jrick@conformal.com>
2014-10-30 20:43:28 -0500

    Signals and back/forward buttons.

--- volo.cpp
+++ volo.cpp
@@ -9,16 +9,38 @@ using namespace volo;
 
 const Glib::ustring blank_page = "about:blank";
 
-WebContext::WebContext(WebKitProcessModel model) : wc{webkit_web_context_get_default()} {
-	set_process_model(model);
+WebContext WebContext::get_default() {
+	return WebContext{};
 }
-
-WebContext::WebContext() : WebContext{WEBKIT_PROCESS_MODEL_MULTIPLE_SECONDARY_PROCESSES} {}
 
 void WebContext::set_process_model(WebKitProcessModel model) {
 	webkit_web_context_set_process_model(wc, model);
 }
 
+
+static void WebView_load_changed_callback(WebKitWebView *wv,
+	WebKitLoadEvent ev, gpointer signal) {
+
+	// TODO: only emit once, no matter how many times this was connected.
+	static_cast<sigc::signal<void, WebKitLoadEvent> *>(signal)->emit(ev);
+}
+
+static void BackForwardList_changed_callback(WebKitBackForwardList *list,
+	WebKitBackForwardListItem *item, gpointer removed, gpointer signal) {
+
+	static_cast<sigc::signal<void, WebKitBackForwardList *> *>(signal)->emit(list);
+}
+
+WebView::WebView(WebKitWebView *wv) : Gtk::Widget{GTK_WIDGET(wv)} {
+	g_signal_connect(wv, "load-changed",
+		G_CALLBACK(WebView_load_changed_callback),
+		&signal_load_changed);
+
+	auto bfl = webkit_web_view_get_back_forward_list(wv);
+	g_signal_connect(bfl, "changed",
+		G_CALLBACK(BackForwardList_changed_callback),
+		&signal_back_forward_list_changed);
+}
 
 WebView::WebView(const Glib::ustring& uri) : WebView{} {
 	load_uri(uri);
@@ -28,18 +50,20 @@ void WebView::load_uri(const Glib::ustring& uri) {
 	webkit_web_view_load_uri(gobj(), uri.c_str());
 }
 
-static void WebView_connect_load_changed_callback(WebKitWebView *wv,
-	WebKitLoadEvent ev, gpointer signal) {
+void WebView::go_back() {
+	webkit_web_view_go_back(gobj());
+};
 
-	// TODO: only emit once, no matter how many times this was connected.
-	static_cast<sigc::signal<void, WebKitLoadEvent> *>(signal)->emit(ev);
-}
+void WebView::go_forward() {
+	webkit_web_view_go_forward(gobj());
+};
 
 sigc::connection WebView::connect_load_changed(std::function<void(WebKitLoadEvent)> handler) {
-	g_signal_connect(gobj(), "load-changed",
-		G_CALLBACK(WebView_connect_load_changed_callback),
-		&signal_load_changed);
 	return signal_load_changed.connect(handler);
+}
+
+sigc::connection WebView::connect_back_forward_list_changed(std::function<void(WebKitBackForwardList *)> handler) {
+	return signal_back_forward_list_changed.connect(handler);
 }
 
 void WebView::on_load_changed(WebKitLoadEvent ev) {
@@ -52,9 +76,10 @@ const std::initializer_list<Glib::ustring> default_session = {
 	"https://github.com",
 };
 
-Browser::Browser() : Browser{default_session} {} // TODO: use about:blank
-
 Browser::Browser(const std::vector<Glib::ustring>& uris) {
+	auto wc = WebContext::get_default();
+	wc.set_process_model(WEBKIT_PROCESS_MODEL_MULTIPLE_SECONDARY_PROCESSES);
+
 	// Set navigation button images.  No gtkmm constructor wraps
 	// gtk_button_new_from_icon_name (due to a conflict with
 	// gtk_button_new_with_label) so this must be done in the Browser
@@ -64,8 +89,12 @@ Browser::Browser(const std::vector<Glib::ustring>& uris) {
 	//stop.set_image_from_icon_name("process-stop");
 	//reload.set_image_from_icon_name("view-refresh");
 
-	navbar.add(back);
-	navbar.add(fwd);
+	auto histnav_style = histnav.get_style_context();
+	histnav_style->add_class("raised");
+	histnav_style->add_class("linked");
+	histnav.add(back);
+	histnav.add(fwd);
+	navbar.add(histnav);
 
 	nav_entry.set_input_purpose(Gtk::INPUT_PURPOSE_URL);
 	nav_entry.set_hexpand(true);
@@ -82,6 +111,10 @@ Browser::Browser(const std::vector<Glib::ustring>& uris) {
 	for (const auto& uri : uris) {
 		open_uri(uri);
 	}
+
+	nb.signal_switch_page().connect([this] (auto, guint page_num) {
+		switch_page(page_num);
+	});
 
 	show_webview(*webviews.front());
 
@@ -110,23 +143,44 @@ int Browser::open_uri(const Glib::ustring& uri) {
 	return n;
 }
 
-void Browser::show_webview(WebView &wv) {
+void Browser::show_webview(WebView& wv) {
+	// Update navbar with the current state of the webview being shown.
+	// These details will be continuously updated with the callbacks set
+	// below until a different webview is shown.
+	update_histnav(wv);
+
 	page_signals = { {
-		back.signal_clicked().connect([] { std::cout << "back\n"; }),
-		fwd.signal_clicked().connect([] { std::cout << "fwd\n"; }),
+		back.signal_clicked().connect([&wv] { wv.go_back(); }),
+		fwd.signal_clicked().connect([&wv] { wv.go_forward(); }),
 		wv.connect_load_changed(sigc::mem_fun(wv, &WebView::on_load_changed)),
+		wv.connect_back_forward_list_changed([&](WebKitBackForwardList *bfl) {
+			if (current_page.bfl != bfl) {
+				std::cout << "oops\n";
+				return;
+			}
+			update_histnav(*current_page.webview);
+		}),
 	} };
-	back.signal_clicked().connect([] { std::cout << "lalala\n"; });
 }
 
-void Browser::switch_webview(WebView &wv) {
+void Browser::update_histnav(WebView& wv) {
+	const auto p = wv.gobj();
+	back.set_sensitive(webkit_web_view_can_go_back(p));
+	fwd.set_sensitive(webkit_web_view_can_go_forward(p));
+}
+
+void Browser::switch_page(uint page_num) {
+	std::cout << "switched page: " << page_num << "\n";
+
 	// Disconnect previous WebView's signals before showing and connecting
 	// the new WebView.
 	for (auto& sig : page_signals) {
 		sig.disconnect();
 	}
 
-	show_webview(wv);
+	auto& wv = webviews.at(page_num);
+	current_page = PageContext{page_num, wv};
+	show_webview(*wv);
 }
 
 
--- volo.h
+++ volo.h
@@ -30,50 +30,77 @@ T* make_managed(Args&&... args) {
 
 // WebContext wraps the default WebKitWebContext.  Non-default contexts may be
 // representable in later versions of WebKitGTK, but as of 2.6, it appears that
-// only the default 
+// only the default context ever exists.
 class WebContext {
 private:
+	// Underlying web context.
 	WebKitWebContext * const wc;
 
 public:
-	WebContext();
-	WebContext(WebKitProcessModel);
+	// get_default returns a WebContext wrapping the default web context.
+	// This is the only way to create a WebContext, since other contexts
+	// other than the default cannot be constructed.
+	static WebContext get_default();
 
+	// set_process_model modifies the process model for a web context.
+	// By default, a web context will use a single process to manage
+	// all WebViews.
+	//
+	// It is unsafe to change the process model after WebViews have been
+	// created.
+	//
+	// See http://webkitgtk.org/reference/webkit2gtk/stable/WebKitWebContext.html#webkit-web-context-set-process-model
+	// for additional details.
 	void set_process_model(WebKitProcessModel);
+
+protected:
+	// Constructor to wrap the default web context.  This is not public
+	// since it may be possible to construct non-default web contexts
+	// in the future.
+	WebContext() : wc{webkit_web_context_get_default()} {}
 };
 
 
 // WebView is a thin wrapper around a WebKitWebView, deriving from a gtkmm
 // widget so it may be used in combination with other GTK widgets.
+//
+// Additional details regarding the methods wrapped by this class can be
+// found at: http://webkitgtk.org/reference/webkit2gtk/stable/WebKitWebView.html
 class WebView : public Gtk::Widget {
 private:
 	// Signals.
 	sigc::signal<void, WebKitLoadEvent> signal_load_changed;
+	sigc::signal<void, WebKitBackForwardList *> signal_back_forward_list_changed;
 
 public:
 	// Constructors to allocate and initialize a new WebKitWebView.  The
 	// default constructor will load the blank about page, but a string
 	// any other URI may be specified to begin loading the resource as the
 	// WebView is created.
-	WebView() : WebView{WEBKIT_WEB_VIEW(webkit_web_view_new())} {}
+	WebView() : WebView{reinterpret_cast<WebKitWebView *>(webkit_web_view_new())} {}
 	WebView(const Glib::ustring&);
 
 	// load_uri begins the loading the URI described by uri in the WebView.
-	//
-	// See http://webkitgtk.org/reference/webkit2gtk/stable/WebKitWebView.html#webkit-web-view-load-uri
-	// for additional details.
 	void load_uri(const Glib::ustring&);
+
+	// go_back loads the previous history item.
+	void go_back();
+
+	// go_forward loads the next history item.
+	void go_forward();
 
 	// Signal connections.
 	sigc::connection connect_load_changed(std::function<void(WebKitLoadEvent)>);
+	sigc::connection connect_back_forward_list_changed(std::function<void(WebKitBackForwardList *)>);
 
 	// Slot..
 	void on_load_changed(WebKitLoadEvent);
 
 protected:
 	// Constructor to create a WebView from a C pointer.
-	WebView(WebKitWebView *wv) : Gtk::Widget{GTK_WIDGET(wv)} {}
+	WebView(WebKitWebView *wv);
 
+public:
 	// Accessor methods for the underlying GObject.
 	WebKitWebView * gobj() { return reinterpret_cast<WebKitWebView *>(gobject_); }
 	const WebKitWebView * gobj() const { return reinterpret_cast<WebKitWebView *>(gobject_); }
@@ -90,30 +117,47 @@ protected:
 // tab will open a new one to the blank page.
 class Browser : public Gtk::Window {
 private:
-	WebContext wc;
+	struct PageContext {
+		uint tab_index{0};
+		WebView *webview{nullptr};
+		WebKitBackForwardList *bfl{nullptr};
+		PageContext() {}
+		PageContext(uint tab_idx, std::unique_ptr<WebView>& wv) {
+			tab_index = tab_idx;
+			webview = wv.get();
+			bfl = webkit_web_view_get_back_forward_list(wv->gobj());
+		}
+	};
+
 	// A vector of WebView pointers is used instead of a vector of WebViews
 	// since no move constructor is defined for WebView and gtkmm classes
 	// from which it derives.  This appears to be a limitation with gtkmm.
 	std::vector<std::unique_ptr<WebView>> webviews;
 	Gtk::HeaderBar navbar;
+	Gtk::Box histnav;
 	Gtk::Button back, fwd, stop, reload;
 	Gtk::Entry nav_entry;
 	Gtk::Notebook nb;
-	std::array<sigc::connection, 3> page_signals;
+	// Details about the currently shown page.
+	std::array<sigc::connection, 4> page_signals;
+	PageContext current_page;
 
 public:
 	// Constructors to create the toplevel browser window widget.  Multiple
 	// URIs (a "session") to open may be specified, while the default
 	// constructor will open a single tab to a single blank page.
-	Browser();
+	//
+	// TODO(jrick): actually use about:blank
+	Browser() : Browser{{"https://duckduckgo.com/lite", "https://github.com"}} {}
 	Browser(const std::vector<Glib::ustring>&);
 
 	// open_uri opens a new tab and begins loading the specified resource.
 	int open_uri(const Glib::ustring&);
 
 private:
-	void show_webview(WebView &);
-	void switch_webview(WebView &);
+	void show_webview(WebView&);
+	void switch_page(uint);
+	void update_histnav(WebView&);
 };
 
 } // namespace volo
