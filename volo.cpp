jrick/volo

commit 1ae3fe3830340ff24cab16aa5d2d9cc10b932fe6

Use a wrapper method rather than a GTK free function.

volo.cpp
@@ -359,8 +359,7 @@ void browser::show_webview(unsigned int page_num, webkit::web_view& wv) {
 
 	// Update navbar/titlebar with the current state of the webview being
 	// shown.
-	auto c_title = webkit_web_view_get_title(wv.ptr());
-	window->set_title(c_title ? c_title : "volo");
+	window->set_title(wv.get_title());
 	update_histnav(wv);
 	auto uri = wv.get_uri();
 	nav_entry->set_uri(uri);

commit 21e4914e4b80dffbb1c870b392233d0c9f71e4a2

Set window title when webview title is empty.
This fixes a regression from 1ae3fe3830340ff24cab16aa5d2d9cc10b932fe6.

volo.cpp
@@ -359,7 +359,11 @@ void browser::show_webview(unsigned int page_num, webkit::web_view& wv) {
 
 	// Update navbar/titlebar with the current state of the webview being
 	// shown.
-	window->set_title(wv.get_title());
+	auto title = wv.get_title();
+	if (!strcmp(title, "")) {
+		title = "volo";
+	}
+	window->set_title(title);
 	update_histnav(wv);
 	auto uri = wv.get_uri();
 	nav_entry->set_uri(uri);